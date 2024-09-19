40555dd3d13d7783912b2430f74c921930a4dac5
Quentin Leboucher <qleboucher1@gmail.com>
2024-09-19 16:19:24 +0200

    Salut pd

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController 
+{
+    #[Route('/')]
+    public function accueil(): Response
+    {
+        $accueil = [
+          'name'=>'Agence Web',
+          'description' =>'Voyager comme vous etes',
+          'valeur' => 'Transparent , Sécurité , Accompagnement'
+        ];
+        return $this->render('accueil.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/destination')]
+    public function destination(): Response
+    {
+        $destination = [
+            'name' => 'Paris',
+            'description' => "C'est beau",
+        ];
+
+        return $this->render('destination.html.twig', [
+            'destination' => $destination,
+        ]);
+    }
+}
